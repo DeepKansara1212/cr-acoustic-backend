5a989cb0e66fb2eb9b63bffbcaefe935186c4ecd
deepkansara1212@gmail.com <deepkansara1212@gmail.com>
2026-09-01 16:30:30 +0530

    Update backend

--- src/app.js
+++ src/app.js
@@ -14,6 +14,8 @@ const { apiLimiter } = require('./middleware/rateLimiter.middleware');
 
 const app = express();
 
+app.set('trust proxy', 1);
+
 app.use(helmet());
 const allowedOrigins = [process.env.CLIENT_URL, process.env.ADMIN_URL];
 
